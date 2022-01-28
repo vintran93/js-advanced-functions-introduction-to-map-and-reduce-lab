20507008331082176d2697ee8a72e6f7e0761058
Vincent Tran <vincentuong@yahoo.com>
2022-01-27 20:49:03 -0600

    commit tree 279dc88bc78d38f33bd115dcc8437d4c2c669f48

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let arr = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        arr.push(-1 * sourceArray[i])
+    }
+    return arr
+}
+
+function mapToNoChange(sourceArray) {
+    let arr = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        arr.push(sourceArray[i])
+    }
+    return arr
+}
+
+function mapToDouble(sourceArray) {
+    let arr = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        arr.push(sourceArray[i] * 2)
+    }
+    return arr
+}
+
+function mapToSquare(sourceArray) {
+    let arr = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        arr.push(sourceArray[i] ** 2)
+    }
+    return arr
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        total = total + sourceArray[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (sourceArray[i]) return true
+    }
+    return false
+}
